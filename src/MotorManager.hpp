3ee73696e71bc49521d3938f016fc288a6693b8d
Julian Liersch <julian.liersch@dfki.de>
2024-03-15 14:23:35 +0100

    MotorManager: Added override keyword

--- src/MotorManager.hpp
+++ src/MotorManager.hpp
@@ -45,7 +45,7 @@ namespace mars
             /**
              * \brief Destructor.
              */
-            virtual ~MotorManager(){}
+            virtual ~MotorManager() override {}
 
             /**
              * \brief Add a motor to the simulation.
@@ -59,13 +59,13 @@ namespace mars
              * \return The unique id of the newly added motor.
              */
             //virtual unsigned long addMotor(interfaces::MotorData *motorS, interfaces::JointInterface *joint, bool reload = false);
-            virtual unsigned long addMotor(interfaces::MotorData *motorS, interfaces::JointInterface *joint, std::string frame, bool reload =  false);
+            virtual unsigned long addMotor(interfaces::MotorData *motorS, interfaces::JointInterface *joint, std::string frame, bool reload =  false) override;
             /**
              *\brief Returns the number of motors that are currently present in the simulation.
              *
              *\return The number of all motors.
              */
-            virtual int getMotorCount() const;
+            virtual int getMotorCount() const override;
 
             /**
              * \brief Change motor properties.
@@ -77,7 +77,7 @@ namespace mars
              * \param motorS The id of the MotorData referred by this pointer must be the
              * same as the id of the motor that is to be edited.
              */
-            virtual void editMotor(const interfaces::MotorData &motorS);
+            virtual void editMotor(const interfaces::MotorData &motorS) override;
 
             /**
              * \brief Gives information about core exchange data for motors.
@@ -86,7 +86,7 @@ namespace mars
              * core_objects_exchange struct for every motor and its index. The vector is cleared
              * in the beginning of this function.
              */
-            virtual void getListMotors(std::vector<interfaces::core_objects_exchange> *motorList) const;
+            virtual void getListMotors(std::vector<interfaces::core_objects_exchange> *motorList) const override;
 
 
             /**
@@ -96,14 +96,14 @@ namespace mars
              *
              * \return A pointer to the MotorData of the motor with the given id.
              */
-            virtual const interfaces::MotorData getFullMotor(unsigned long index) const;
+            virtual const interfaces::MotorData getFullMotor(unsigned long index) const override;
 
             /**
              * \brief Removes a motor from the simulation.
              *
              * \param index The unique id of the motor to remove form the simulation.
              */
-            virtual void removeMotor(unsigned long index);
+            virtual void removeMotor(unsigned long index) override;
 
             /**
              * \brief This function returns the SimMotor object for a given id.
@@ -117,7 +117,7 @@ namespace mars
              *
              * \returns Returns a pointer to the corresponding motor object.
              */
-            virtual SimMotor* getSimMotor(unsigned long id) const;
+            virtual SimMotor* getSimMotor(unsigned long id) const override;
 
             /**
              * \brief This function returns the SimMotor object for a given name.
@@ -131,7 +131,7 @@ namespace mars
              *
              * \returns Returns a pointer to the corresponding motor object.
              */
-            virtual SimMotor* getSimMotorByName(const std::string &name) const;
+            virtual SimMotor* getSimMotorByName(const std::string &name) const override;
 
             /**
              * \brief Sets the value of the motor with the given id to the given value.
@@ -144,7 +144,7 @@ namespace mars
              *
              * \param value The new value.
              */
-            virtual void setMotorValue(unsigned long id, interfaces::sReal value);
+            virtual void setMotorValue(unsigned long id, interfaces::sReal value) override;
 
             /**
              * \brief Sets the maximum torque of the motor with the given id to the given value.
@@ -153,15 +153,15 @@ namespace mars
              *
              * \param maxTorque The new maximum torque for the motor.
              */
-            virtual void setMaxTorque(unsigned long id, interfaces::sReal maxTorque);
-            virtual void setMaxSpeed(unsigned long id, interfaces::sReal maxSpeed);
+            virtual void setMaxTorque(unsigned long id, interfaces::sReal maxTorque) override;
+            virtual void setMaxSpeed(unsigned long id, interfaces::sReal maxSpeed) override;
 
             /**
              * \brief Sets the desired speed of a motor.
              * \param id The id of the motor whose value is to be changed.
              * \param value The new value in rad/s.
              */
-            virtual void setMotorValueDesiredVelocity(unsigned long id, interfaces::sReal velocity);
+            virtual void setMotorValueDesiredVelocity(unsigned long id, interfaces::sReal velocity) override;
 
             /**
              * \brief Sets the proportional term of the motor with the given id to the given value.
@@ -174,7 +174,7 @@ namespace mars
              *
              * \param value The new P value.
              */
-            virtual void setMotorP(unsigned long id, interfaces::sReal value);
+            virtual void setMotorP(unsigned long id, interfaces::sReal value) override;
 
             /**
              * \brief Sets the integral term of the motor with the given id to the given value.
@@ -187,7 +187,7 @@ namespace mars
              *
              * \param value The new I value.
              */
-            virtual void setMotorI(unsigned long id, interfaces::sReal value);
+            virtual void setMotorI(unsigned long id, interfaces::sReal value) override;
 
             /**
              * \brief Sets the derivative term of the motor with the given id to the given value.
@@ -200,14 +200,14 @@ namespace mars
              *
              * \param value The new D value.
              */
-            virtual void setMotorD(unsigned long id, interfaces::sReal value);
+            virtual void setMotorD(unsigned long id, interfaces::sReal value) override;
 
             /**
              * \brief Deactivates the motor with the given id.
              *
              * \param id The id of the motor that is to be deactivated.
              */
-            virtual void deactivateMotor(unsigned long id);
+            virtual void deactivateMotor(unsigned long id) override;
 
             /**
              * \brief Destroys all motors in the simulation.
@@ -218,7 +218,7 @@ namespace mars
              * \param clear_all Indicates if the reload motors should
              * be destroyed as well. If set to \c false they are left intact.
              */
-            virtual void clearAllMotors(bool clear_all = false);
+            virtual void clearAllMotors(bool clear_all = false) override;
 
             /**
              * \brief This function reloads all motors from a temporary motor pool.
@@ -226,7 +226,7 @@ namespace mars
              * \details All motors that have been added with \c reload value as \c true
              * are added back to the simulation again with a \c reload value of \c true.
              */
-            virtual void reloadMotors(void);
+            virtual void reloadMotors(void) override;
 
             /**
              * \brief This function updates all motors with timing value \c calc_ms in miliseconds.
@@ -236,19 +236,19 @@ namespace mars
              *
              * \param calc_ms The timing value in miliseconds.
              */
-            virtual void updateMotors(interfaces::sReal calc_ms);
+            virtual void updateMotors(interfaces::sReal calc_ms) override;
 
             /**
              * \returns the actual position of the motor with the given Id.
              *          returns 0 if a motor with the given Id doesn't exist.
              */
-            virtual interfaces::sReal getActualPosition(unsigned long motorId) const;
+            virtual interfaces::sReal getActualPosition(unsigned long motorId) const override;
 
             /**
              * \returns the torque excerted by the motor with the given Id.
              *          returns 0 if a motor with the given Id doesn't exist.
              */
-            virtual interfaces::sReal getTorque(unsigned long motorId) const;
+            virtual interfaces::sReal getTorque(unsigned long motorId) const override;
 
             /**
              * \brief Sets the value of the motor with the given id to the given value.
@@ -261,7 +261,7 @@ namespace mars
              *
              * \param value The new value.
              */
-            virtual void moveMotor(unsigned long index, interfaces::sReal value);
+            virtual void moveMotor(unsigned long index, interfaces::sReal value) override;
 
             /**
              * \brief Detaches the joint with the given index from all motors that act on
@@ -273,7 +273,7 @@ namespace mars
              *
              * \param joint_index The id of the joint that is to be detached.
              */
-            virtual void removeJointFromMotors(unsigned long joint_index);
+            virtual void removeJointFromMotors(unsigned long joint_index) override;
 
             /**
              * \brief Retrieves the id of a motor by name
@@ -282,17 +282,17 @@ namespace mars
              *
              * \return Id of the motor if it exists, otherwise 0
              */
-            virtual unsigned long getID(const std::string& motor_name) const;
+            virtual unsigned long getID(const std::string& motor_name) const override;
 
             virtual void getDataBrokerNames(unsigned long jointId,
                                             std::string *groupName,
-                                            std::string *dataName) const;
+                                            std::string *dataName) const override;
 
-            virtual void connectMimics();
+            virtual void connectMimics() override;
             virtual void setOfflinePosition(interfaces::MotorId id,
                                             interfaces::sReal pos);
             virtual void edit(interfaces::MotorId id, const std::string &key,
-                              const std::string &value);
+                              const std::string &value) override;
 
         private:
             std::shared_ptr<envire::core::EnvireGraph> envireGraph;
