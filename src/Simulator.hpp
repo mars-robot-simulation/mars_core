e4d3f9c59f1e0c81793fe651534d9c4f74cf9dc1
Malte Langosz <malte.langosz@dfki.de>
2024-11-26 16:35:44 +0100

    Remove envire search function (moved to mars_interfaces)

--- src/Simulator.hpp
+++ src/Simulator.hpp
@@ -294,49 +294,6 @@ namespace mars
                                            utils::Vector linearVelocity,
                                            std::shared_ptr<envire::core::EnvireGraph> &envireGraph,
                                            std::shared_ptr<envire::core::TreeView> &graphTreeView);
-            template<typename T>
-            static envire::core::Item<T>& searchForTopItem(std::shared_ptr<envire::core::EnvireGraph> &envireGraph,
-                                                           std::shared_ptr<envire::core::TreeView> &graphTreeView,
-                                                           envire::core::FrameId startNode,
-                                                           envire::core::FrameId *foundNode=nullptr)
-                {
-                    bool done = false;
-                    envire::core::FrameId frame = startNode;
-                    bool found = false;
-                    envire::core::Item<T> rT;
-                    while(!done)
-                    {
-                        try
-                        {
-                            auto& it = envireGraph->getItem<envire::core::Item<T>>(frame);
-                            if(foundNode != nullptr)
-                            {
-                                *foundNode = frame;
-                            }
-                            return *it;
-                        }
-                        catch (...)
-                        {
-                        }
-                        if(!done)
-                        {
-                            const auto& vertex = envireGraph->vertex(frame);
-                            const auto& parentVertex = graphTreeView->tree[vertex].parent;
-                            // todo: check if this check is correct
-                            if(parentVertex)
-                            {
-                                frame = envireGraph->getFrameId(parentVertex);
-                            }
-                            else
-                            {
-                                done = true;
-                            }
-                        }
-                    }
-                    throw envire::core::UnknownFrameException(startNode);
-                    return rT;
-                }
-
             interfaces::sReal getStepSizeS() const override;
             virtual interfaces::sReal getVectorCollision(utils::Vector position, utils::Vector ray);
 
